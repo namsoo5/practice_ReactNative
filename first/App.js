a65ccd44f73d0ab17a7b6ecedcf8ec3a6bde9c5c
namsoo <are34@naver.com>
2019-01-25 16:38:54 +0900

    flatlist생성

--- first/App.js
+++ first/App.js
@@ -28,15 +28,31 @@ class App extends Component {
     };
   }
 
-  _renderItem = 
+  // _renderItem = data => {
+  //   return <Text style={styles.row}>{data.item.key}</Text>;
+  // };
+
+  _renderItem = ({item}) => {
+    return <Text style={styles.row}>{item.key}</Text>;
+  };
 
   render() {
     return (
-      <div>
-        
-      </div>
+      <View style={styles.container}>
+        <FlatList data={this.state.data} renderItem={this._renderItem} />
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  row: { fontSize: 24, padding: 42, borderWidth: 1, borderColor: "#DDDDDD" }
+});
 
 export default App;

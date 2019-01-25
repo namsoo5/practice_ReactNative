10c5d76b7a3d4e39c6c87446ca0874cbd1aa8bbc
namsoo <are34@naver.com>
2019-01-25 16:33:58 +0900

    flatlist연습시작

---
+++ first/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+import { StyleSheet, Text, View, FlatList } from "react-native";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { key: "a" },
+        { key: "b" },
+        { key: "c" },
+        { key: "d" },
+        { key: "a longer example" },
+        { key: "e" },
+        { key: "f" },
+        { key: "g" },
+        { key: "h" },
+        { key: "i" },
+        { key: "j" },
+        { key: "k" },
+        { key: "l" },
+        { key: "m" },
+        { key: "n" },
+        { key: "o" },
+        { key: "p" }
+      ]
+    };
+  }
+
+  _renderItem = 
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
